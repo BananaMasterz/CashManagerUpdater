58e5c4aea036b65dcbeb93e7e0463c135cff6890
BananaMasterz <papadakisk96@gmail.com>
2021-04-06 21:13:36 +0300

    k

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 with open('Release.zip', 'rb') as file_data:
     bytes_content = file_data.read()
     # print(bytes_content)
-with open('test.txt', 'wb') as f:
+with open('release.txt', 'wb') as f:
     f.write(bytes_content)
